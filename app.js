80b53e9dad386058def9cd9ef5a715371c340ce4
hanan320 <Hanoona3-20@hotmail.com>
2024-03-20 08:56:44 +0000

    redy to save

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-function myFunction() {
-    
+document.addEventListener('DOMContentLoaded', function() {
+    // Ask user for name
     var name = prompt("Please enter your name:");
 
-    
-    var gender = prompt("Please enter your gender (male or female):");
+    // Ask user for gender
+    var gender = prompt("Please enter your gender (M/m==male or F/f=female):");
 
-    
+    // Ask user for age and validate
     var age = parseInt(prompt("Please enter your age:"));
     if (age <= 0) {
         alert("Age cannot be less than or equal to zero.");
     }
 
-    
+    // Ask user to confirm skipping welcoming message
     var skip_Welcome = confirm("Do you want to skip the welcoming message?");
 
-   
+    // Display welcoming message
     if (!skip_Welcome) {
         var welcomeMessage = "Welcome";
-        if (gender.toLowerCase() === 'male') {
+        if (gender.toLowerCase() === 'm') {
             welcomeMessage += " Mr";
-        } else if (gender.toLowerCase() === 'female') {
+        } else if (gender.toLowerCase() === 'f') {
             welcomeMessage += " Ms";
         }
         welcomeMessage += " " + name;
         alert(welcomeMessage);
     }
-}
+});
